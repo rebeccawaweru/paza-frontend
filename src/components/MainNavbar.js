8b41b09de2bb9bfeba0f7c6a9780494416df9469
IsmaelNjama <petermailsnjama@gmail.com>
2024-05-29 01:32:43 +0300

    aligned navbar contents

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,19 +1,60 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 
 const MainNavbar = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">PAZA</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mL-auto">
-          <Nav.Link href="#home">Profile</Nav.Link>
-          <Nav.Link href="#link">Job board</Nav.Link>
-          <Nav.Link href="#link">Showcase</Nav.Link>
-          <Nav.Link href="#link">Dashboard</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#home" className="d-flex align-items-center">
+          <img
+            src="/paza.png"
+            height="30"
+            className="d-inline-block align-top mr-2"
+            alt=""
+          />
+          PAZA
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Container className="d-flex justify-content-center">
+            <Nav>
+              <Nav.Link href="#home">Profile</Nav.Link>
+              <Nav.Link href="#link">Job board</Nav.Link>
+              <Nav.Link href="#link">Showcase</Nav.Link>
+              <Nav.Link href="#link">Dashboard</Nav.Link>
+            </Nav>
+          </Container>
+          <Nav className="ml-auto">
+            <Nav.Link href="#search">
+              <i className="bi bi-search"></i>
+            </Nav.Link>
+            <Nav.Link href="#messages">
+              <i className="bi bi-chat-dots"></i>
+            </Nav.Link>
+            <Nav.Link href="#notifications">
+              <i className="bi bi-bell"></i>
+            </Nav.Link>
+            <Nav.Link href="#settings">
+              <i className="bi bi-gear"></i>
+            </Nav.Link>
+            <NavDropdown
+              title={
+                <img
+                  src="path-profile-ing"
+                  height="30"
+                  className="rounded-circle"
+                  alt="Profile"
+                />
+              }
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item href="#profile">Profile name</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };

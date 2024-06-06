21b51bb90ff4acd389e918e4682a19b513f913c4
rebeccawaweru <wawerur95@gmail.com>
2024-06-07 00:51:56 +0300

    complete signup page

--- src/layouts/Wrapper.jsx
+++
@@ -1,3 +0,0 @@
-export default function Wrapper(){
-    return <>Wrapper for major components eg dashboard</>
-}
--- src/pages/Auth/Signup/Signup.jsx
+++ src/pages/Auth/Signup/Signup.jsx
@@ -1,3 +1,76 @@
-export default function Login(){
-    return <p>Signup</p>
+import { Radio, FormControlLabel, RadioGroup } from "@mui/material";
+import { BasicButton, BasicInput } from "../../../components"
+import { AuthWrapper } from "../../../layouts";
+import { useState } from "react";
+import {useNavigate} from 'react-router-dom'
+export default function Signup(){
+    const radiocls = {
+        color: "gray",
+        '&.Mui-checked': {
+          color: "#FB5607",
+        },
+      }
+    const navigate = useNavigate()
+    const [visible,setVisible] = useState(false);
+    const [values,setValues] = useState({
+        firstname:"",
+        lastname:"",
+        email:"",
+        birthday:"",
+        gender:"",
+        phone:"",
+        city:"",
+        password:"",
+        confirmpassword:""
+    });
+    const handleChange = (e) => {
+       setValues((prev) => ({...prev, [e.target.name]:e.target.value}))
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(values)
+
+    }
+    return <AuthWrapper navbtn={<BasicButton custom="px-8 text-sm" title="Log in" handleClick={() => navigate('/')}/>}>
+    <div className="flex flex-col text-center items-center justify-center space-y-2 mt-24 pb-8">
+    <p className="text-xl">Please Fill Up These Details</p>
+    <p className="text-zinc-600 font-bold">How are you planning to use Paza? We’ll fit the experience to your needs.</p> 
+    <p className="text-zinc-600 font-bold"> Don’t worry, you’ll be able to change this later on</p>
+    <form onSubmit={handleSubmit} className="w-full flex flex-col items-center space-y-6 pt-4 px-4 2xl:px-0 xl:px-0 lg:px-0 md:px-0">
+    <BasicInput type="text" name="firstname" onChange={handleChange} required placeholder="First Name"/>
+    <BasicInput type="text" name="lastname" onChange={handleChange} required placeholder="Last Name"/>
+    <BasicInput type="email" name="email" onChange={handleChange} required placeholder="Email"/>
+    <div className="w-full 2xl:w-1/2 xl:w-1/3 lg:w-1/3 md:w-1/2 sm:w-2/3 grid grid-cols-2 ">
+    <BasicInput custom="w-full date-input" type="text"  onFocus={(e) => (e.target.type = "date")} name="birthday" onChange={handleChange} required placeholder="Birthday" end="bi bi-calendar text-white right-6 absolute z-10"/>
+    <RadioGroup
+        className="w-full mx-6 flex items-center justify-evenly"
+        row
+        aria-labelledby="demo-gender-label"
+        defaultValue="female"
+        name="gender"
+        onChange={handleChange}
+    >
+    
+    <FormControlLabel className="border border-orange-500 rounded-full px-2 w-24 h-8" value="male"
+    control={<Radio sx={radiocls} size="extrasmall" />}
+    label={<span style={{ fontSize: 'small' }}>Male</span>}
+    sx={{ alignItems: 'center'}} // ensure vertical alignment
+    />
+    <FormControlLabel className="border border-orange-500 rounded-full pr-2 w-26 h-8" value="female"
+    control={<Radio sx={radiocls} size="extrasmall" />}
+    label={<span style={{ fontSize: 'small' }}>Female</span>}
+    sx={{ alignItems: 'center' }} // ensure vertical alignment
+    />
+    </RadioGroup>
+    </div>
+    <div className="w-full 2xl:w-1/2 xl:w-1/3 lg:w-1/3 md:w-1/2 sm:w-2/3 grid grid-cols-2 gap-2">
+    <BasicInput custom="w-full" type="text" name="phone" onChange={handleChange} required placeholder="+254" start="bi bi-telephone text-white"/>
+    <BasicInput custom="w-full" type="text" name="city" onChange={handleChange} required placeholder="City" start="bi bi-geo-alt-fill text-white"/>
+    </div>
+    <BasicInput name="password" onChange={handleChange}  show={()=>setVisible(!visible)} type={visible ? "text" : "password"} required placeholder="Password" start="bi bi-search text-white" end={`${visible ? 'bi bi-eye' :'bi bi-eye-slash' }  text-white`}/>
+    <BasicInput name="confirmpassword" onChange={handleChange}  show={()=>setVisible(!visible)} type={visible ? "text" : "password"} required placeholder="Confirm Password" start="bi bi-search text-white" end={`${visible ? 'bi bi-eye' :'bi bi-eye-slash' }  text-white`}/>
+    <BasicButton title="Sign up" custom="w-full 2xl:w-1/2 xl:w-1/3 lg:w-1/3 md:w-1/2 sm:w-2/3 text-lg"/>
+    </form>
+    </div>
+    </AuthWrapper>
 }

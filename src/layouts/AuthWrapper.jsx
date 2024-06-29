a6c03285a0ea67ca1d4e16b934bc9e2a99b152fe
IsmaelNjama <petermailsnjama@gmail.com>
2024-06-30 00:53:59 +0300

    fix:upload to aws-s3

--- src/layouts/AuthWrapper.jsx
+++ src/layouts/AuthWrapper.jsx
@@ -1,36 +1,69 @@
-import { createContext,useReducer } from "react";
-import { pazadark } from "../assets"
+import { createContext, useReducer } from "react";
+import { pazadark } from "../assets";
 import { reducer2 } from "../utils/helpers";
-export const AuthContext = createContext()
-export default function AuthWrapper({children,navbtn,initialValues}){
-    const [state2, dispatch] = useReducer(reducer2,initialValues)
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        dispatch({ type: 'HANDLE_CHANGE', payload: { name, value } });
-    };
-    const handleSelect = (name,value) => {
-        if (state2.subCategory.length > 0) {
-            dispatch({type:"HANDLE_CHANGE", payload:{name:'subCategory', value:[]}})
+import axios from "axios";
+export const AuthContext = createContext();
+export default function AuthWrapper({ children, navbtn, initialValues }) {
+  const [state2, dispatch] = useReducer(reducer2, initialValues);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    dispatch({ type: "HANDLE_CHANGE", payload: { name, value } });
+  };
+  const handleSelect = (name, value) => {
+    if (state2.subCategory.length > 0) {
+      dispatch({
+        type: "HANDLE_CHANGE",
+        payload: { name: "subCategory", value: [] },
+      });
+    }
+    dispatch({ type: "HANDLE_CHANGE", payload: { name, value } });
+  };
+  const handleAdd = (arrayName, value) => {
+    dispatch({ type: "ADD", payload: { arrayName, value } });
+  };
+  const handleRemove = (arrayName, value) => {
+    dispatch({ type: "REMOVE", payload: { arrayName, value } });
+  };
+  const handleAvatar = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("avatar", file);
+
+    try {
+      const res = await axios.post(
+        "http://54.90.77.200:5000/uploads",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
         }
-        dispatch({type:'HANDLE_CHANGE', payload:{name,value}})
-        
+      );
+      console.log(res);
+      const avatarUrl = res.data.avatarUrl;
+      dispatch({ type: "UPLOAD_SUCCESS", payload: { value: avatarUrl } });
+    } catch (error) {
+      console.error("Error uploading avatar", error);
     }
-    const handleAdd = (arrayName,value) => {
-        dispatch({type:'ADD', payload: {arrayName, value}})
-    }
-    const handleRemove = (arrayName,value)=> {
-        dispatch({type:'REMOVE', payload: {arrayName, value}})
-    }
-    const handleAvatar = (e) => {
-        const file = e.target.files[0]
-        dispatch({type:'UPLOAD', payload:{file}})
-    }
-    return <AuthContext.Provider value={{state2,handleChange,handleAdd,handleRemove,handleAvatar,handleSelect}}><div className="min-h-screen w-full flex flex-col justify-center relative overflow-hidden ">
-    <div className="w-full absolute top-0 p-3 flex  justify-between">
-    <img src={pazadark} alt="pazalogo" className="w-36 object-contain" />
-    {navbtn}
-    </div>
-    {children}
-    </div>
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        state2,
+        handleChange,
+        handleAdd,
+        handleRemove,
+        handleAvatar,
+        handleSelect,
+      }}
+    >
+      <div className="min-h-screen w-full flex flex-col justify-center relative overflow-hidden ">
+        <div className="w-full absolute top-0 p-3 flex  justify-between">
+          <img src={pazadark} alt="pazalogo" className="w-36 object-contain" />
+          {navbtn}
+        </div>
+        {children}
+      </div>
     </AuthContext.Provider>
+  );
 }
--- src/utils/helpers.jsx
+++ src/utils/helpers.jsx
@@ -1,117 +1,159 @@
-import client from "../api/client"
-import { toast } from "react-toastify"
+import client from "../api/client";
+import { toast } from "react-toastify";
 export const initialState = {
-    step:1
-}
-export const reducer = (state=initialState,action) => {
-    switch(action.type){
-        case 'Next':
-            return {...state, step: state.step + 1}
-        case 'Prev':
-            return {...state, step: state.step - 1}
-        case 'Reset':
-            return initialState
-        default:
-            return state
-    }
-}
+  step: 1,
+};
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "Next":
+      return { ...state, step: state.step + 1 };
+    case "Prev":
+      return { ...state, step: state.step - 1 };
+    case "Reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 export const creator = [
-    {
-        name:"Visual Arts",
-        category:['Painters', 'Digital Artists', 'Illustrators','Graffiti Artists','3D Artists', 'Designers','Photographers']
-    },
-    {
-        name:"Media Art",
-        category:['Filmmakers','Animators', 'Gamers','streamers','Podcasters', 'Vloggers']
-    },
-    {
-        name:'Music',
-        category:['Singers', 'Songwriters', 'Instrumentalists', 'Composers', 'DJs']
-    },
-    {
-        name:'Performing Arts',
-        category:['Actors', 'Comedians', 'Dancers', 'Other Live Performers']
-    },
-    {
-        name:'Fashion and Beauty',
-        category:['Fashion Designers', 'Stylists', 'Hair and Makeup Artists', 'Beauty Influencers']
-    },
-    {
-        name:'Writers, Editors, and Bloggers',
-        category:['Writers','Editors','Bloggers']
-    },
-    {
-        name:'Health and Wellness',
-        category:['Health','Fitness','Wellness']
-    },
-    {
-        name:'Personality',
-        category:['Social Media Influencers','Brand Ambassadors','Content Creator']
-    },
-    {
-        name:'Merchandise and Crafts',
-        category:['Merchandise','Handicrafts','Artisanal Products']
-    },
-    {
-        name:'Niche Professionals',
-        category:['Chefs', 'Developers', 'Analyst', 'Researcher', 'Consultant']
-    },
-    {
-        name:'Lifestyle and Hobbies',
-        category:['Food Bloggers','Travel Bloggers']
-    }
-]
+  {
+    name: "Visual Arts",
+    category: [
+      "Painters",
+      "Digital Artists",
+      "Illustrators",
+      "Graffiti Artists",
+      "3D Artists",
+      "Designers",
+      "Photographers",
+    ],
+  },
+  {
+    name: "Media Art",
+    category: [
+      "Filmmakers",
+      "Animators",
+      "Gamers",
+      "streamers",
+      "Podcasters",
+      "Vloggers",
+    ],
+  },
+  {
+    name: "Music",
+    category: [
+      "Singers",
+      "Songwriters",
+      "Instrumentalists",
+      "Composers",
+      "DJs",
+    ],
+  },
+  {
+    name: "Performing Arts",
+    category: ["Actors", "Comedians", "Dancers", "Other Live Performers"],
+  },
+  {
+    name: "Fashion and Beauty",
+    category: [
+      "Fashion Designers",
+      "Stylists",
+      "Hair and Makeup Artists",
+      "Beauty Influencers",
+    ],
+  },
+  {
+    name: "Writers, Editors, and Bloggers",
+    category: ["Writers", "Editors", "Bloggers"],
+  },
+  {
+    name: "Health and Wellness",
+    category: ["Health", "Fitness", "Wellness"],
+  },
+  {
+    name: "Personality",
+    category: [
+      "Social Media Influencers",
+      "Brand Ambassadors",
+      "Content Creator",
+    ],
+  },
+  {
+    name: "Merchandise and Crafts",
+    category: ["Merchandise", "Handicrafts", "Artisanal Products"],
+  },
+  {
+    name: "Niche Professionals",
+    category: ["Chefs", "Developers", "Analyst", "Researcher", "Consultant"],
+  },
+  {
+    name: "Lifestyle and Hobbies",
+    category: ["Food Bloggers", "Travel Bloggers"],
+  },
+];
 
-export const reducer2 = (state,action)=>{
-    switch(action.type){
-       case 'HANDLE_CHANGE':
-           return {
-               ...state,
-               [action.payload.name]:action.payload.value
-           }
-       case 'ADD':
-           return {
-               ...state,
-               [action.payload.arrayName]: [
-                   ...state[action.payload.arrayName],
-                   action.payload.value,
-               ],    
-           }
-       case 'REMOVE':
-           return {
-               ...state,
-               [action.payload.arrayName]:state[action.payload.arrayName].filter(item => item !== action.payload.value)
-           }
-       case 'UPLOAD':
-           return {
-               ...state,
-               avatar:URL.createObjectURL(action.payload.file)
-           }
-       default:
-           return state;
-    }
-}
+export const reducer2 = (state, action) => {
+  switch (action.type) {
+    case "HANDLE_CHANGE":
+      return {
+        ...state,
+        [action.payload.name]: action.payload.value,
+      };
+    case "ADD":
+      return {
+        ...state,
+        [action.payload.arrayName]: [
+          ...state[action.payload.arrayName],
+          action.payload.value,
+        ],
+      };
+    case "REMOVE":
+      return {
+        ...state,
+        [action.payload.arrayName]: state[action.payload.arrayName].filter(
+          (item) => item !== action.payload.value
+        ),
+      };
+    case "UPLOAD":
+      return {
+        ...state,
+        avatar: URL.createObjectURL(action.payload.file),
+      };
+    case "UPLOAD_SUCCESS":
+      return {
+        ...state,
+        avatar: action.payload.value,
+      };
+    default:
+      return state;
+  }
+};
 
 export const Validity = (e) => {
-    const formElement = e.target;
-    const isValid = formElement.checkValidity();
-    return isValid
-}
+  const formElement = e.target;
+  const isValid = formElement.checkValidity();
+  return isValid;
+};
 
-export const handleUpdate = (state2,navigate) => {
-    const user = JSON.parse(localStorage.getItem('user')) || ''
-    const token = localStorage.getItem('token')
-    client.put(`/users/${user._id}`, { account: state2 }, 
-        { headers: { Authorization: `${token}` } }
-    ).then((response)=>{
-      if(response.data.acknowledged) {
-        toast.success('Account details updated')
-        setTimeout(()=>{
-            navigate('/overview')
-        },3000)
+export const handleUpdate = (state2, navigate) => {
+  const user = JSON.parse(localStorage.getItem("user")) || "";
+  const token = localStorage.getItem("token");
+  client
+    .put(
+      `/users/${user._id}`,
+      { account: state2 },
+      { headers: { Authorization: `${token}` } }
+    )
+    .then((response) => {
+      if (response.data.acknowledged) {
+        toast.success("Account details updated");
+        setTimeout(() => {
+          navigate("/overview");
+        }, 3000);
       }
-   }).catch((err)=>{
-      toast.error(err.message)
-   })
-}
+    })
+    .catch((err) => {
+      toast.error(err.message);
+    });
+};
